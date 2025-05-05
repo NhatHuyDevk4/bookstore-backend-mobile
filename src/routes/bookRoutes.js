bc8d96a7b5f885d66cdf02671f33612ac4efa6bf
Lê Nhật Huy <99423466+NhatHuyDevk4@users.noreply.github.com>
2025-05-05 23:50:45 +0700

    Done get and delete (#4)

--- src/routes/bookRoutes.js
+++ src/routes/bookRoutes.js
@@ -4,8 +4,8 @@ import Book from '../model/Book.js';
 import protectRoute from '../middleware/auth.middleware.js';
 
 const router = express.Router();
-// create 
 
+// create 
 router.post("/", protectRoute, async (req, res) => {
     try {
 
@@ -36,6 +36,79 @@ router.post("/", protectRoute, async (req, res) => {
 
     } catch (error) {
         console.error("Error creating book:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+})
+
+// get
+router.post("/", protectRoute, async (req, res) => {
+    try {
+        const page = req.query.page || 1; // Lấy số trang từ query string, mặc định là 1
+        const limit = req.query.limit || 5; // Lấy số lượng bản ghi trên mỗi trang từ query string, mặc định là 5
+        const skip = (page - 1) * limit; // Tính toán số bản ghi cần bỏ qua
+
+        const books = await Book.find({})
+            .sort({ createdAt: -1 })
+            .skip(skip) // Bỏ qua số bản ghi đã tính toán
+            .limit(limit) // Giới hạn số bản ghi trả về
+            .populate("user", "username profileImage") // Thay thế ID người dùng bằng thông tin người dùng
+
+        const totalBooks = await Book.countDocuments(); // Đếm tổng số bản ghi trong collection
+        res.send(
+            {
+                books,
+                currentPage: page,
+                totalBooks,
+                totalPages: Math.ceil(totalBooks / limit), // Tính toán tổng số trang
+            }
+        );
+
+    } catch (error) {
+        console.error("Error getting all books:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+})
+
+// recommended
+router.get("/user", protectRoute, async (req, res) => {
+    try {
+        const books = await Book.find({ user: req.user._id }).sort({ createdAt: -1 }).limit(5); // Lấy 5 sách có rating cao nhất
+        res.json(books);
+    } catch (error) {
+        console.error("Error getting recommended books:", error);
+        res.status(500).json({ message: "Internal server error" });
+    }
+})
+
+// [DELETE]/[id]
+router.delete("/:id", protectRoute, async (req, res) => {
+
+    try {
+        const book = await Book.findById(req.params.id);
+        if (!book) {
+            return res.status(404).json({ message: "Book not found" });
+        }
+
+        // Kiểm tra nếu người dùng là người tạo sách
+        if (book.user.toString() !== req.user._id.toString()) {
+            return res.status(403).json({ message: "Unauthorized" });
+        }
+
+        // Xóa ảnh khỏi Cloudinary nếu cần thiết
+        if (book.image && book.image.includes("cloudinary")) {
+            try {
+                const publicId = book.image.split("/").pop().split(".")[0]; // Lấy public ID từ URL
+                await cloudinary.uploader.destroy(publicId); // Xóa ảnh khỏi Cloudinary
+            } catch (deleteError) {
+                console.error("Error deleting image from Cloudinary:", deleteError);
+                return res.status(500).json({ message: "Failed to delete image from Cloudinary" });
+            }
+        }
+
+        await Book.deleteOne();
+        res.json({ message: "Book deleted successfully" });
+    } catch (error) {
+        console.error("Error deleting book:", error);
         res.status(500).json({ message: "Internal server error" });
     }
 })
