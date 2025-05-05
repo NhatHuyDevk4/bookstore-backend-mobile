ef22e89d581f8099726d6dd202f7dc61cee7dddc
NhatHuyDevk4 <lenhathuy9a6@gmail.com>
2025-05-05 22:37:32 +0700

    Done flow register

--- index.js
+++ index.js
@@ -6,6 +6,10 @@ import { connectDB } from './src/lib/db.js';
 
 
 const app = express();
+// Thêm middleware này để xử lý JSON trong request body
+app.use(express.json());
+// Nếu cần nhận form-url-encoded (không bắt buộc cho JSON)
+app.use(express.urlencoded({ extended: true }));
 const PORT = process.env.PORT || 3000;
 
 app.use("/api/auth", authRoutes);
--- src/model/User.js
+++ src/model/User.js
@@ -1,6 +1,7 @@
 
 import mongoose from "mongoose";
-
+import bcrypt from "bcryptjs";
+// Đoạn code này sử dụng mongoose để kết nối và tương tác với MongoDB, và bcryptjs để mã hóa mật khẩu người dùng.
 const userSchema = new mongoose.Schema({
     username: {
         type: String,
@@ -23,8 +24,16 @@ const userSchema = new mongoose.Schema({
     }
 });
 
-const User = mongoose.model("User", userSchema); 
-// Này là cách tạo model trong mongoose, nó sẽ tự động tạo collection trong MongoDB với tên là "users" (số nhiều của "user").
+userSchema.pre("save", async function (next) {
+    // if(this.isModified("password")) {
+    //     return next();
+    // } // Nếu mật khẩu không thay đổi thì không cần mã hóa lại modified là đã sửa đổi
+    const salt = await bcrypt.genSalt(10);
+    this.password = await bcrypt.hash(this.password, salt);
+    next();
+})
 
+const User = mongoose.model("User", userSchema);
+// Này là cách tạo model trong mongoose, nó sẽ tự động tạo collection trong MongoDB với tên là "users" (số nhiều của "user").
 export default User;
 // Đoạn code này định nghĩa một schema cho người dùng với các trường như username, email, password và profileImage.
